12691b6354b7e9d76880a6b1c4a0b42a322f4d5e
@dlcanegro <garciaolivo89@gmail.com>
2021-10-13 17:06:34 -0300

    orginize code

--- controllers/controller.js
+++ controllers/controller.js
@@ -1,133 +1,65 @@
-//backend functions
-
-//var fileupload = require('express-fileupload');
 const sharp = require("sharp");
 const fs = require("fs");
-const path = require("path");
-const fileItem=require("./item")
-
-
-let fileName = "";
-let onlyName='';
-let urlDest; 
+const fileItem = require("./item");
 let item;
+let res=null;
 
 exports.genThumbnail = (req, res) => {
 
-  try{
-    
-    let file = req.files.files; 
-    item= new fileItem(file)
-
-    let fn=item.getFullName()
-    if(fn) item.setFullName(fn)
-    let woext=item.getNameWithOutExtension(fn)
-    if(woext) item.setName(woext)
-    let ext=item.getExtension(fn)
-    if(ext){
-      item.setExtension(ext.extension)
-      item.setType(ext.type)
-    } 
-
-    let newDir = createDir()
-    if (fs.existsSync(newDir)) {
-      let newDirFile=`${newDir}/${item.getFullName()}`
-      
-      item.setDirFullPath(newDirFile)
-      file.mv(newDirFile,  (err, result)=> {
-        
-        createThumbnail(newDirFile)
-
-       }); 
+  try {
+    res=res
+    let file = req.files.files;
+    item = new fileItem(file);
+    let fn = item.getFullName();
+    if (fn) item.setFullName(fn);
+    let woext = item.getNameWithoutExtension(fn);
+    if (woext) item.setName(woext);
+    let ext = item.getExtension(fn);
+    if (ext) {
+      item.setExtension(ext.extension);
+      item.setType(ext.type);
     }
-    
-  }
-  catch(err){
+    let newDir = item.createDir();
+    if (fs.existsSync(newDir)) {
+      let newDirFile = `${newDir}/${item.getFullName()}`;
+      item.setDirFullPath(newDirFile);
+      file.mv(newDirFile, (err, result) => {
+        createThumbnail(newDirFile);
+      });
+    }
+  } catch (err) {
     console.log(err);
   }
-  
-  
-  function createThumbnail(pathFile){
-    switch (item.type) {
-      case 'image':
-          newImageThumbnail();
-        break;
-      case 'video':
-          newVideoThumbnail(pathFile);
-        break;
-    }
-  }
 
-  function newVideoThumbnail(){
-      generateVideoThumbnail().then(()=>{
-        let b64=item.base64_encode(`${item.getDirPath()}/thumb_${item.getName()}.png`)
-        item.setBase64(b64)
-      }).finally(()=>{
-        console.log('base64 file saved');
-        item.removeTempPath()
-        console.log(item);
-      })
 
-  }
 
-  function newImageThumbnail(){
-    generateImageThumbnail()
+function createThumbnail() {
+  switch (item.type) {
+    case "image":
+      newImageThumbnail();
+      break;
+    case "video":
+      newVideoThumbnail();
+      break;
   }
-  
-  
-};
-
-function createDir(){
-  let newNameDir=new Date().getTime();
-  var dir = __dirname + `/${newNameDir}`;
-  item.setDirPath(dir)
-  if (!fs.existsSync(dir)) {
-      fs.mkdirSync(dir, 0744);
-  }
-  return dir;
 }
 
-function generateVideoThumbnail(){
-  return new Promise((resolve,reject)=>{
-        const ffmpegInstaller = require('@ffmpeg-installer/ffmpeg');
-        const ffmpeg = require('fluent-ffmpeg');
-        ffmpeg.setFfmpegPath(ffmpegInstaller.path);
-        // console.log(ffmpegInstaller.path, ffmpegInstaller.version);
-        var pathToFile=item.getDirFullPath()
-        var pathToSnapshot = item.getDirPath()
-        var proc = ffmpeg(pathToFile)
-        .on('filenames', (filenames)=> {
-          console.log('screenshots are ' + filenames.join(', '));
-        })
-        .on('end', (data)=> {
-          console.log('screenshots were saved');
-        })
-        .on('error', (err)=> {
-          console.log('an error happened: ' + err.message);
-          return reject(new Error(err))
-        })
-        // take 2 screenshots at predefined timemarks and size
-        //.takeScreenshots({ count: 2, timemarks: [ '00:00:02.000', '6' ], size: '150x100' },pathToSnapshot);
-        .takeScreenshots({ count: 1,filename:`thumb_${item.getName()}.png`, timemarks: [ '00:00:01.000' ], size: '250x?' },pathToSnapshot)
-          .on('end', () => {
-            console.log('thumb video done!')
-            resolve()
-          })
-          .on('error', (err)=> {
-            console.log('an error happened: ' + err.message);
-            return reject(new Error(err))
-          })   
-    })      
+function newVideoThumbnail() {
+  generateVideoThumbnail()
+    .then(() => {
+      let b64 = item.base64_encode(
+        `${item.getDirPath()}/thumb_${item.getName()}.png`
+      );
+      item.setBase64(b64);
+    })
+    .finally(() => {
+      console.log("base64 video created!");
+      item.removeTempPath();
+      sendThumbnailToFront()
+    });
 }
 
-
-
-
-
-
-
-function generateImageThumbnail() {
- 
+function newImageThumbnail() {
   let newUrlDest = item.getDirFullPath();
   try {
     sharp(newUrlDest)
@@ -136,29 +68,60 @@ function generateImageThumbnail() {
       .toBuffer()
       .then((data) => {
         const base64Data = data.toString("base64");
-        item.setBase64(base64Data)
-        console.log('base64 file saved');
-        item.removeTempPath()
-        
+        item.setBase64(base64Data);
+        console.log("base64 image created!");
+        item.removeTempPath();
+        sendThumbnailToFront()
       });
   } catch (error) {
     console.log(error);
   }
 }
 
-
-
-function sendThumbnail(base64Data, res) {
-  res.status(202).json({ b64Data: base64Data, extension: "png" });
+function generateVideoThumbnail() {
+  return new Promise((resolve, reject) => {
+    const ffmpegInstaller = require("@ffmpeg-installer/ffmpeg");
+    const ffmpeg = require("fluent-ffmpeg");
+    ffmpeg.setFfmpegPath(ffmpegInstaller.path);
+    // console.log(ffmpegInstaller.path, ffmpegInstaller.version);
+    var pathToFile = item.getDirFullPath();
+    var pathToSnapshot = item.getDirPath();
+    var proc = ffmpeg(pathToFile)
+      .on("filenames", (filenames) => {
+        console.log("screenshots are " + filenames.join(", "));
+      })
+      .on("end", (data) => {
+        console.log("screenshots were saved");
+      })
+      .on("error", (err) => {
+        console.log("an error happened: " + err.message);
+        return reject(new Error(err));
+      })
+      // take 2 screenshots at predefined timemarks and size
+      //.takeScreenshots({ count: 2, timemarks: [ '00:00:02.000', '6' ], size: '150x100' },pathToSnapshot);
+      .takeScreenshots(
+        {
+          count: 1,
+          filename: `thumb_${item.getName()}.png`,
+          timemarks: ["00:00:01.000"],
+          size: "250x?",
+        },
+        pathToSnapshot
+      )
+      .on("end", () => {
+        console.log("thumb video done!");
+        resolve();
+      })
+      .on("error", (err) => {
+        console.log("an error happened: " + err.message);
+        return reject(new Error(err));
+      });
+  });
 }
 
 
+function sendThumbnailToFront() {
+  res.status(202).json({ state:'ok',item:item });
+}
 
-
-
-
-
-
-
-
-
+};
--- controllers/item.js
+++ controllers/item.js
@@ -1,129 +1,115 @@
-const sharp = require("sharp");
 const fs = require("fs");
-const path = require("path");
-const fileItem=require("./item")
 
-class Item{
-    constructor(file){
-        this.fullname=null;
-        this.name=null;
-        this.extension=null;
-        this.type=null;
-        this.file=file;
-        this.base64=null;
-        this.dirPath=null;
-        this.dirFullPath=null;
+class Item {
+  constructor(file) {
+    this.fullname = null;
+    this.name = null;
+    this.extension = null;
+    this.type = null;
+    this.file = file;
+    this.base64 = null;
+    this.dirPath = null;
+    this.dirFullPath = null;
+  }
+
+  //Fullname
+  setFullName(name) {
+    this.fullname = name;
+  }
+  getFullName() {
+    return this.file.name;
+  }
+
+  //Name
+  setName(name) {
+    this.name = name;
+  }
+  getName() {
+    return this.name;
+  }
+
+  //DitPath
+  setDirPath(dir) {
+    this.dirPath = dir;
+  }
+  getDirPath() {
+    return this.dirPath;
+  }
+
+  //DirFullPath
+  setDirFullPath(dir) {
+    this.dirFullPath = dir;
+  }
+  getDirFullPath() {
+    return this.dirFullPath;
+  }
+
+  //Type
+  setType(type) {
+    this.type = type;
+  }
+  getType() {
+    return this.type;
+  }
+
+  //Base 64
+  setBase64(b64) {
+    this.base64 = b64;
+  }
+  getBase64() {
+    return this.base64;
+  }
+  base64_encode(file) {
+    var bitmap = fs.readFileSync(file);
+    let b64str = new Buffer.from(bitmap).toString("base64");
+    return b64str;
+  }
+
+  //Extension
+  setExtension(ext) {
+    this.extension = ext;
+  }
+  getExtension(fullname) {
+    let result_extension = {};
+    const images = ["jpg", "gif", "png"];
+    const videos = ["mp4", "3gp", "ogg", "avi"];
+    let indexPoint = fullname.lastIndexOf(".");
+    let cantChar = fullname.length - 1;
+    let rest = cantChar - indexPoint;
+    let extension = fullname.substr(-1 * rest);
+    if (images.includes(extension)) {
+      result_extension = { type: "image", extension: extension };
+    } else if (videos.includes(extension)) {
+      result_extension = { type: "video", extension: extension };
     }
 
-    
-    setFullName(name){
-      this.fullname=name
-    }
-    getFullName(){
-        return this.file.name;
-    }
+    return result_extension;
+  }
 
-    
+  getNameWithoutExtension(fullname) {
+    let indexPoint = fullname.lastIndexOf(".");
+    let name = fullname.substr(0, indexPoint);
+    return name;
+  }
 
-    setName(name){
-      this.name=name
-    }
-    getName(){
-      return this.name
-    }
-
-    setDirPath(dir){
-      this.dirPath=dir
-    }
-    getDirPath(){
-      return this.dirPath
-
-    }
-
-   
-
-    setDirFullPath(dir){
-      this.dirFullPath=dir
-    }
-
-    getDirFullPath(){
-      return this.dirFullPath
-    }
-
-    
-
-    setType(type){
-      this.type=type
-    }
-
-    setExtension(ext){
-      this.extension=ext
-    }
-
-    setBase64(b64){
-      this.base64=b64
-    }
-
-    getExtension(fullname) {
-        let result_extension = {};
-        const images = ["jpg", "gif", "png"];
-        const videos = ["mp4", "3gp", "ogg", "avi"];
-        let indexPoint = fullname.lastIndexOf(".");
-        let cantChar = fullname.length - 1;
-        let rest = cantChar - indexPoint;
-        let extension = fullname.substr(-1 * rest);
-        if (images.includes(extension)) {
-          result_extension = { type: "image", extension: extension };
-        } else if (videos.includes(extension)) {
-          result_extension = { type: "video", extension: extension };
-        }
-        
-        return result_extension;
+  removeTempPath() {
+    let dir = this.dirPath;
+    fs.rm(dir, { recursive: true }, (err) => {
+      if (err) {
+        throw err;
       }
+    });
+  }
 
-      base64_encode(file) {
-        var bitmap = fs.readFileSync(file);
-        let b64str=new Buffer.from(bitmap).toString('base64');
-        return b64str;
-      }
-
-      getNameWithOutExtension(fullname) {
-        
-        let indexPoint = fullname.lastIndexOf(".");
-        let name = fullname.substr(0,indexPoint);
-        
-        return name;
-      }
-
-      removeTempFiles() {
-
-        const directory = `${__dirname}/tempfiles/`;
-      
-        fs.readdir(directory, (err, files) => {
-          files.forEach((element) => {
-            fs.unlink(path.join(directory, element), (err) => {
-              if (err) throw err;
-            });
-          });
-        });
-      
-      }
-
-      removeTempPath() {
-        let dir=this.dirPath
-        fs.rmdir(dir, { recursive: true }, (err) => {
-          if (err) {
-              throw err;
-          }
-        
-      
-      })
+  createDir() {
+    let newNameDir = new Date().getTime();
+    var dir = __dirname + `/${newNameDir}`;
+    this.setDirPath(dir);
+    if (!fs.existsSync(dir)) {
+      fs.mkdirSync(dir);
     }
-
-      
-
-      
-
+    return dir;
+  }
 }
 
 module.exports = Item;
